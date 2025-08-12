e3417057021fddd76c8e70be5f187d7e0b4a370c
esjung <esjung@woobosys.com>
2025-08-12 11:32:42 +0900

    🎨 glb 파일, 컴포넌트 업데이트 (position 개선)

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { JSX } from 'react';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, PerspectiveCamera } from '@react-three/drei';
 import { GLTF } from 'three-stdlib';
 
 type GLTFResult = GLTF & {
@@ -16,31 +16,37 @@ export function Model(props: JSX.IntrinsicElements['group']) {
 	const { nodes, materials } = useGLTF('/ci.glb') as unknown as GLTFResult;
 	return (
 		<group {...props} dispose={null}>
-			<group scale={0.01}>
-				<group position={[0, 159.177, 0]}>
-					<group position={[-122, 153, -25.01]}>
-						<mesh
-							castShadow
-							receiveShadow
-							geometry={nodes.Shape_0.geometry}
-							material={nodes.Shape_0.material}
-							position={[27.457, 0, 0]}
-						/>
-						<mesh
-							castShadow
-							receiveShadow
-							geometry={nodes.Shape_1.geometry}
-							material={nodes.Shape_1.material}
-							position={[59.852, -35.156, 0.01]}
-						/>
-						<mesh
-							castShadow
-							receiveShadow
-							geometry={nodes.Shape_2.geometry}
-							material={nodes.Shape_2.material}
-							position={[0.004, -73.427, 0.02]}
-						/>
-					</group>
+			<group scale={0.03}>
+				<PerspectiveCamera
+					makeDefault={false}
+					far={100000}
+					near={70}
+					fov={45}
+					position={[-194.685, 182.83, 795.333]}
+					rotation={[-0.354, -0.164, -0.06]}
+				/>
+				<group position={[-122, 153, -25.01]}>
+					<mesh
+						castShadow
+						receiveShadow
+						geometry={nodes.Shape_0.geometry}
+						material={nodes.Shape_0.material}
+						position={[27.457, 0, 0]}
+					/>
+					<mesh
+						castShadow
+						receiveShadow
+						geometry={nodes.Shape_1.geometry}
+						material={nodes.Shape_1.material}
+						position={[59.852, -35.156, 0.01]}
+					/>
+					<mesh
+						castShadow
+						receiveShadow
+						geometry={nodes.Shape_2.geometry}
+						material={nodes.Shape_2.material}
+						position={[0.004, -73.427, 0.02]}
+					/>
 				</group>
 			</group>
 		</group>
